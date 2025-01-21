4a9b609e4c44916facd25e31d803e172f5c85db3
shamsrza <rzsams@hotmail.com>
2025-01-21 14:33:18 +0100

    some container alignments

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -38,7 +38,7 @@ const SignUpPage = () => {
 
 
   return (
-  <div className="min-h-screen grid lg:grid-cols-2 mt-5">
+  <div className="min-h-screen grid lg:grid-cols-2">
     {/* left side of form */}
     <div className="flex flex-col justify-center items-center p-6 sm:p-12">
       <div className="w-full max-w-md space-y-8">
